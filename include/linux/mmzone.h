b1a15af66219848fbd71525214275d1d5bee2655
Leoysen <leoyzen@gmail.com>
2013-10-18 03:41:43 +0800

    Add UKSM 1.2.2 and replace KSM
    Change-Id: I1b82e7af4825fd9215a586d22f9af2910a3ad8ef
    Signed-off-by: Leoysen <leoyzen@gmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -116,6 +116,9 @@ enum zone_stat_item {
 	NUMA_OTHER,		/* allocation from other node */
 #endif
 	NR_ANON_TRANSPARENT_HUGEPAGES,
+#ifdef CONFIG_UKSM
+	NR_UKSM_ZERO_PAGES,
+#endif
 	NR_VM_ZONE_STAT_ITEMS };
 
 /*
@@ -373,6 +376,9 @@ struct zone {
 	/* Fields commonly accessed by the page reclaim scanner */
 	spinlock_t		lru_lock;
 	struct lruvec		lruvec;
+	struct zone_lru {
+		struct list_head list;
+	} lru[NR_LRU_LISTS];
 
 	struct zone_reclaim_stat reclaim_stat;
 
@@ -754,7 +760,7 @@ static inline int is_normal_idx(enum zone_type idx)
 }
 
 /**
- * is_highmem - helper function to quickly check if a struct zone is a 
+ * is_highmem - helper function to quickly check if a struct zone is a
  *              highmem zone or not.  This is an attempt to keep references
  *              to ZONE_{DMA/NORMAL/HIGHMEM/etc} in general code to a minimum.
  * @zone - pointer to struct zone variable
